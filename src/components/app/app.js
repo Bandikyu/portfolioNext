f22bb0647bca52e84f565d86e1456ffb868f80bb
Bru <bripoll.18@gmail.com>
2022-05-23 20:55:40 -0300

    haciendo que un estado sea mas general

---
+++ src/components/app/app.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import '../../index.css';
+
+import Front from '../front/front';
+import Header from '../header/header';
+
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.throttle = this.throttle.bind(this);
+        this.coordY = this.coordY.bind(this);
+        this.state = {scroll: window.scrollY , throttleTimer: false};
+    }
+    coordY() {
+      if(this.state.throttleTimer === true) {
+        return this.setState({scroll: window.scrollY}) 
+      }else return;
+    };
+    
+    throttle (callback, time) {
+      if (this.state.throttleTimer) return;
+      
+      this.setState({throttleTimer: true})
+      
+      setTimeout(() => {
+          callback();
+          this.setState({throttleTimer: false});
+      }, time);
+    }
+
+    render() {
+        window.addEventListener('scroll' , ()=>this.throttle(this.coordY , 300));
+        
+        return (
+          <div className='Princial'>
+            <Front/>
+            <Header scroll={this.state.scroll}/>
+          </div>
+        );
+    }
+}
+
+export default App;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,48 +6,13 @@ import Button from '../button/button'
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.throttle = this.throttle.bind(this);
-    this.coordY = this.coordY.bind(this);
-    this.state = {/* classHead: 'header' , classNav: 'nav', */ scroll: window.scrollY , throttleTimer: false};
   }
-  coordY() {
-    if(this.state.throttleTimer === true) {
-      return this.setState({scroll: window.scrollY}) 
-    }else return;
-  };
- 
-  throttle (callback, time) {
-    //don't run the function while throttle timer is true
-    if (this.state.throttleTimer) return;
-    
-    //first set throttle timer to true so the function doesn't run
-    //this.state.throttleTimer = true;
-    this.setState({throttleTimer: true})
-    
-    setTimeout(() => {
-        //call the callback function in the setTimeout and set the throttle timer to false after the indicated time has passed 
-        callback();
-        this.setState({throttleTimer: false});
-        //console.log(this.state.scroll)
-        //this.state.throttleTimer = false;
-    }, time);
-  }
+
   
-  render() {
-    /*       setTimeout(() =>(
-      window.scrollY<100 ? 
-      this.setState({classHead:'header' , classNav:'nav'})  : 
-      this.setState({classHead:'header header-srll' , classNav:'nav nav-srll'})
-      ) , 300) */
-      window.addEventListener('scroll' , ()=>this.throttle(this.coordY , 300));
-      
-      
+  render() {      
       return (
-        
-        //<div id='header' className={this.state.classHead}>
-        <div id='header' className={this.state.scroll>=100 ? 'header header-srll' : 'header'}>
-          {/* <Nav name={this.state.ClassNav}/> */}
-          <Nav name={this.state.scroll>=100 ? 'nav nav-srll' : 'nav'}/>
+        <div id='header' className={this.props.scroll>=100 ? 'header header-srll' : 'header'}>
+          <Nav name={this.props.scroll>=100 ? 'nav nav-srll' : 'nav'}/>
         </div>
       );
     }
