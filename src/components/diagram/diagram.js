be9823bc55d0661c536e11c0cf30ee63ac7d957f
Bru <bripoll.18@gmail.com>
2022-05-28 21:14:12 -0300

    changes 1

--- src/components/diagram/diagram.js
+++ src/components/diagram/diagram.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import {img} from '../../img/img';
+import './diagram.css'
+import {imgs} from '../../img/img';
 
 function Diagram(props) {
-    console.log(img.imgJS);
+    let imgDiagrams = [];
+/*     for(let img in imgs) {
+        imgDiagrams.push(
+            <li><img src={imgs[img]}/>{imgs.img}</li>
+        );
+    } */
+    
+    //console.log(array);
     return (
-        <div>Soy un diagrama
-            <img src={img.imgJS}/>
-        </div>
+        <ul className='diagrams-container'>
+            {/* {imgDiagrams[2]} */}
+            <li className='diagram'>
+                <h3>CSS</h3>
+                <ul>
+                    <li>Sintaxis</li>
+                    <li>Selectors</li>
+                    <li>Cascada y Herencia</li>
+                    <li>Pseudo-Elemento</li>
+                    <li>Pseudo-Clase</li>
+                    <li>Transition</li>
+                    <li>Transform</li>
+                    <li>Funciones</li>
+                    <li>BackGrounds and Borders</li>
+                    <li>Position</li>
+                    <li>FlexBox</li>
+                    <li>Grid</li>
+                </ul>
+                <img src={imgs.imgCSS}/>
+            </li>
+        </ul>
     )
 }
 
--- src/img/img.js
+++ src/img/img.js
@@ -1,4 +1,4 @@
-const img = {
+const imgs = {
     imgJS: 'https://db3pap006files.storage.live.com/y4m48dynY7b9F2LEewFQo8RDwIRI8wpAq0_aFhEvNy2qW4A1Yt-j-MT81moM_qWLFZVRjcL7rBltKQP08ucuNhsffJq9B4FAk5Gk_S4ep3OA4Cumd7zl7fMbDGNZOUx3v5JxWCS-G1sHsMu4k89vRXt5fMVQhQIeElaEuW-ObcoJg24NQB4Qti3ZhJmI3tEbXTm?width=512&height=512&cropmode=none',
     imgMySQL: 'https://db3pap006files.storage.live.com/y4m1Q0Iekys6t7OIfTiq3GybIkNduYXniJUfy0Iej69PbYUBQ_qeU0NTRNepbUgR14zvEA64KC3cyz1A_qPMPJsv8Iii3GL_k-S1qr7vZQrkvjVM-xcXE3tit2jftziD35CRxhZ3SY8YP7gk2orMOQ8ppOpBglTthG9WwRPzJSqPjdf2mcBYxKmVP_nmv1jgfcP?width=512&height=512&cropmode=none',
     imgReact: 'https://db3pap006files.storage.live.com/y4mbpP5yHEfn02Of_3Ao_dq4uwktChd70Tr629vnvVmVt8w6pSv8w6geTn_G1PrWzfUGSTNr5NroQEh87Lb5XtVqFU_Hesz3VVr3fGWgz4DF5afE_HHWSFyTZnWxrhPjEJYw3NEvzfr_ruH6d0Vv9YCiBye9pnOAEcmoWbdp4SKGJhUqBpcYd1-eNzQxWP2IZ1d?width=512&height=512&cropmode=none',
@@ -8,4 +8,4 @@ const img = {
     
 }
 
-export {img};
+export {imgs};
