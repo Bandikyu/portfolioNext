ba6f7c04553a372a495a8923adf059c6a77e5d43
Bru <bripoll.18@gmail.com>
2022-06-17 19:13:04 -0300

    completando estilo de bloques

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const domains = ["db3pap006files.storage.live.com" , "geekland.eu"];
 const nextConfig = {
-    reactStrictMode: true,
+  reactStrictMode: true,
+  images: {
+    domains: domains
+  },
+  env: {
+    DOMAINS_NEXT_CONFIG: domains
   }
-  // import {imgUrls} from './pages/posts/[id]'🤠
-  
-  module.exports = {
-    nextConfig,
-    images: {
-        domains: ["db3pap006files.storage.live.com"],
-      }
-  }
+}
+// import {imgUrls} from './pages/posts/[id]'🤠
+
+module.exports = nextConfig;
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,9 +6,37 @@ import { createBlocks } from '../../lib/generateBlocks';
 import {posts} from '../../lib/posts'
 import {rutesIds} from '../../lib/rutesIds'
 
+// export let imgUrls = ["geekland.eu"];//🤠
+
 
 export default function Post({ response }) {
+
     let bloques = createBlocks(response);
+/*     async function cosa() {
+      process.env.IMG_URL = await JSON.stringify(["geekland.eu"]);
+      let jsonParse = await JSON.parse(process.env.IMG_URL);
+      console.log(jsonParse);
+    };
+    cosa(); */
+    
+/*     console.log(process.env.IMG_URL.split(','));
+    console.log(': %j' , process.env.IMG_URL); */
+/*     console.time('imageURLs');
+    (() => {
+      //considerar que esto es para imges externas
+      let imgsUrls = [];
+      response.resChildrens.results.forEach(e => {
+        if (e.type === 'image') {
+          // console.log(e[e.type])
+          let url = e[e.type][e[e.type].type].url;
+          let cleanUrl = url.match(/\/\/(.*?)\//)[1];
+          imgsUrls.push(cleanUrl);
+          // console.log(url.slice(url.indexOf('://')+3));
+          console.log(typeof cleanUrl);
+        }
+      });
+    })();
+    console.timeEnd('imageURLs'); */
 
     return (
       <section className={styles.container}>
