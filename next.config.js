b7136081a6bc1e5c949240fc593e223a04fa1754
Bandikyu <bripoll.19@gmail.com>
2022-06-18 16:28:59 -0300

    cambio de var de entorno

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ const nextConfig = {
     domains: domains
   },
   env: {
-    DOMAINS_NEXT_CONFIG: domains
+    DOMAINS_NEXT_CONFIG: domains,
+    ID_BASE_PAGE: "5f28676954394485a6db3de0b592a862" //dev
+    // ID_BASE_PAGE: "523abbcb04c541349b2e039fa24c2518" //pro
   }
 }
 // import {imgUrls} from './pages/posts/[id]'🤠
