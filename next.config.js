5c41da84a5139bc9ae7583a5d7e9c14a4dfcf80a
Bandikyu <bripoll.19@gmail.com>
2022-06-20 22:33:09 -0300

    e

--- next.config.js
+++ next.config.js
@@ -7,11 +7,12 @@ const nextConfig = {
     domains: domains
   },
   env: {
-    URL_PAG: "http://localhost:3000" ,
+    // URL_PAG: "http://localhost:3000" , //dev
+    URL_PAG: "portfolio-next-mu.vercel.app" ,
     DOMAINS_NEXT_CONFIG: domains,
-    ID_BASE_PAGE: "fcc50d15-3a08-43bd-a526-2d0329f53c89" //dev
+    // ID_BASE_PAGE: "fcc50d15-3a08-43bd-a526-2d0329f53c89" //dev
     // ID_BASE_PAGE: "5f28676954394485a6db3de0b592a862" //dev
-    // ID_BASE_PAGE: "523abbcb04c541349b2e039fa24c2518" //pro
+    ID_BASE_PAGE: "523abbcb04c541349b2e039fa24c2518" //pro
   }
 }
 // import {imgUrls} from './pages/posts/[id]'🤠
