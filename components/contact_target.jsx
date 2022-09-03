a4195981fa856f6e8ad3fdfce32edc829210acd7
Bandikyu <bripoll.19@gmail.com>
2022-09-03 15:11:03 -0300

    mensajes

--- components/contact_target.jsx
+++ components/contact_target.jsx
@@ -11,19 +11,28 @@ export default function ContactTarget(props) {
             console.log(stateStyle);
         } else {setStateStyle(styles.targetContact)}
     }
+/*     function submitHandler(data) {
+        fetch('/api/sheetsApi', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+        alert("veamos"); 
+    } */
 
     return (
         <div className={stateStyle}>
             <h2>{props.title}</h2>
             {props.mail ? (
-                <form id='form' className={styles.form}>
+                <form action='/api/sheetsApi' method='POST' id='form' className={styles.form}>
                     <input onClick={handleClick} value='✖' type='button'/>
-                    <input placeholder='Email' type='mail' id='email'/>
-                    <textarea id='message'></textarea>
+                    <input placeholder='Email' type='mail' id='email' name='email'/>
+                    <textarea id='message' name='message'></textarea>
                     <input value='Enviar' type='submit'/>
                 </form>
             ) : false}
-
             <div className={styles.tgContactImg}>
                 <Image alt={props.title} src={props.img} layout='fill'/>
             </div>
